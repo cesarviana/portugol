be7280c06298b30d49e1e572225c3398c92c1304
Cesar Viana <cesar.pviana@gmail.com>
2016-10-25 12:00:18 -0200

    Funcionando testes

--- test/ide/impl/compiler/assembly/AssemblerTest.java
+++ test/ide/impl/compiler/assembly/AssemblerTest.java
@@ -235,39 +235,24 @@ public class AssemblerTest {
         add("programa_inicio_vet3 : 0,0,0");
         add(".text");
         add("_PRINCIPAL:");
-        
-        // carrega a
-        add("LDI 2");
-        add("STO programa_inicio_a");
-        // guarda o indice onde o vet2 deve receber o valor
+        add("LDI 2");                       // carrega a
+        add("STO programa_inicio_a");       // guarda o indice onde o vet2 deve receber o valor
         add("LDI 0");
-        add("STO 1000");
-        // guarda o valor da variavel a na pilha
+        add("STO 1000");                    // guarda o valor da variavel a na pilha
         add("LD programa_inicio_a");
-        add("STO 1001");
-        // carrega para $indr o valor do indice guardado 
+        add("STO 1001");                    // carrega para $indr o valor do indice guardado
         add("LD 1000");
-        add("STO $indr");
-        // salva no vetor o valor da variavel a guardado na pilha
+        add("STO $indr");                   // salva no vetor o valor da variavel a guardado na pilha
         add("LD 1001");
         add("STOV programa_inicio_vet2");
-        
-        // carrega a
-        add("LDI 2");
-        add("STO programa_inicio_a");
-        // guarda o indice onde o vet3 deve receber o valor
         add("LDI 0");
         add("STO 1000");
-        // guarda o valor da variavel a na pilha
-        add("LD programa_inicio_a");
+        add("LD programa_inicio_a");        // guarda o valor da variavel a na pilha
         add("STO 1001");
-        // carrega para $indr o valor do indice guardado 
-        add("LD 1000");
-        add("STO $indr");
-        // salva no vetor o valor da variavel a guardado na pilha
+        add("LD 1000");                     // carrega para $indr o valor do indice guardado
+        add("STO $indr");                   // salva no vetor o valor da variavel a guardado na pilha
         add("LD 1001");
         add("STOV programa_inicio_vet3");
-        
         add("HLT 0");
         generateAssemblyAndAssert(code);
     }
@@ -392,12 +377,32 @@ public class AssemblerTest {
     }
 
     @Test
-    public void testOperacaoAritmeticaVetorRecebendoValor(){
+    public void testVetorRecebendoOperacaoAritmetica(){
         String code = " programa{" +
                 "           funcao inicio(){" +
-                "               " +
+                "               inteiro vet[5]" +
+                "               inteiro a = 4" +
+                "               vet[2] = a + 5" +
                 "           }" +
                 "} ";
+        add(".data");
+        add("programa_inicio_vet : 0,0,0,0,0");
+        add("programa_inicio_a : 0");
+        add(".text");
+        add("_PRINCIPAL:");
+        add("LDI 4");
+        add("STO programa_inicio_a");
+        add("LDI 2");
+        add("STO 1000"); // Armarzena indice 2 do vetor na pilha[0]
+        add("LD programa_inicio_a");     // Calcula expressão
+        add("ADDI 5");
+        add("STO 1001"); // Armazena resultado da expressão  na pilha[1]
+        add("LD 1000");  // Carrega indice do vetor (pilha[0])
+        add("STO $indr");// Coloca indice do vetor na variável especial $indr
+        add("LD 1001");  // Carrega resultado da expressão calculado para ACC
+        add("STOV programa_inicio_vet"); // Transfere o resultado do ACC pro vet
+        add("HLT 0");
+        generateAssemblyAndAssert(code);
     }
 
     private void generateAssemblyAndAssert(String code) {
